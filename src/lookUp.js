aac07c8784032e803f2d09e6c97e0b3ddd17b918
啾咪 <122900171@qq.com>
2023-12-02 22:59:25 +0800

    add lookUp module

---
+++ src/lookUp.js
@@ -0,0 +1,19 @@
+export default function lookUp (data,key){
+   
+    if(key.indexOf('.') == -1) return data[key]
+
+    const keyArray = key.split('.')
+    let tempObj = data
+    keyArray.forEach(item => {
+        tempObj = tempObj[item]
+    });
+    return tempObj
+}
+const obj = {
+    a:{
+        b:{
+            c:'到底了'
+        }
+    }
+}
+console.log(lookUp(obj,'a.b.c'));
--- src/renderTemplate.js
+++ src/renderTemplate.js
@@ -1,13 +1,15 @@
+import lookUp from "./lookUp";
 export default function renderTemplate(tokens, data) {
   const resultStr = "";
-  console.log('323',data);
+  //   console.log('323',data);
   const result = tokens.reduce((upper, item) => {
     if (item[0] === "text") {
       upper += item[1];
     } else if (item[0] === "name") {
-      upper += data[item[1]]
+      // 将对象嵌套属性取出
+      upper += lookUp(data, item[1]);
     }
     return upper;
   }, resultStr);
-  console.log('hahhah',result);
+  console.log("hahhah", result);
 }
