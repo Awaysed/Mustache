756002b4e33f8d1dfe6efa01717a732af8d7d9ef
啾咪 <122900171@qq.com>
2023-12-07 23:29:44 +0800

    update:optimize

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ window.SStemplate = {
   render(templateStr, deta) {
     //扫描器
     const tokens = parseTemplateToTokens(templateStr)
-
     console.log(tokens);
     renderTemplate(tokens,deta)
   }
